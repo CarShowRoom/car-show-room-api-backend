e0ea18279c7b799c2106a694363cf672940bca0e
acehidan <aungaungoogame2@gmail.com>
2026-05-25 15:06:41 +0630

    fix: fix order time err

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -387,7 +387,12 @@ export const createOrder = asyncErrorHandler(async (req, res, next) => {
           };
 
           if (orderDate) {
-            orderData.createdAt = new Date(orderDate);
+            const parsed = new Date(orderDate);
+            if (!isNaN(parsed.getTime())) {
+              const now = new Date();
+              parsed.setHours(now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds());
+              orderData.createdAt = parsed;
+            }
           }
 
           newOrder = await Order.create([orderData], { session });
